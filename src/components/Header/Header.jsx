f66f17c8406da22cf8684e4dead72425509417ce
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2022-12-16 16:26:14 +0400

    feat/sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,63 +1,86 @@
 import logo from "../../images/logo.png";
 import profileIcon from "../../images/profile-icon.svg";
+import burgerIcon from "../../images/burger-menu-icon.svg";
 import { Link } from "react-router-dom";
+import SideBar from "../SideBar/SideBar";
+import { useState } from "react";
 
 export default function Header(props) {
   const { isLogged } = props;
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
   return (
     <header className="header">
-      <Link to="/">
-        <img src={logo} alt="Логотип сайта." className="header__logo" />
-      </Link>
-      <div className="header__nav">
-        <div className="header__buttons header__buttons_left">
-          <Link
-            to="/movies"
-            className={`header__buttton ${
-              !isLogged && "header__button_hidden"
-            }`}
-          >
-            Фильмы
-          </Link>
-          <Link
-            to="/saved-movies"
-            className={`header__buttton ${
-              !isLogged && "header__button_hidden"
-            }`}
-          >
-            Сохранённые фильмы
-          </Link>
+      <nav className="header__nav">
+        <Link to="/">
+          <img src={logo} alt="Логотип сайта." className="header__logo" />
+        </Link>
+        <div className="header__nav-container">
+          <div className="header__buttons header__buttons_left">
+            <Link
+              to="/movies"
+              className={`header__buttton ${
+                !isLogged && "header__button_hidden"
+              }`}
+            >
+              Фильмы
+            </Link>
+            <Link
+              to="/saved-movies"
+              className={`header__buttton ${
+                !isLogged && "header__button_hidden"
+              }`}
+            >
+              Сохранённые фильмы
+            </Link>
+          </div>
+          <div className="header__buttons header__buttons_right">
+            {!isLogged ? (
+              <>
+                <Link
+                  to="/signup"
+                  className="header__buttton header__buttton_text_small"
+                >
+                  Регистрация
+                </Link>
+                <Link
+                  to="/signin"
+                  className="header__buttton header__buttton_text_small header__buttton_style_green"
+                >
+                  Войти
+                </Link>
+              </>
+            ) : (
+              <>
+                <div className="header__profile-container">
+                  <Link to="/profile" className="header__buttton">
+                    Аккаунт
+                  </Link>
+                  <img
+                    className="header__profile-icon"
+                    src={profileIcon}
+                    alt="иконка кнопки профиля пользователя."
+                  />
+                </div>
+                <button className="header__burger-button" onClick={openSideBar}>
+                  <img
+                    src={burgerIcon}
+                    alt="Кнопка разворачивания меню."
+                    className="header__burger-icon"
+                  />
+                </button>
+              </>
+            )}
+          </div>
         </div>
-        <div className="header__buttons header__buttons_right">
-          {!isLogged ? (
-            <>
-              <Link
-                to="/signup"
-                className="header__buttton header__buttton_text_small"
-              >
-                Регистрация
-              </Link>
-              <Link
-                to="/signin"
-                className="header__buttton header__buttton_text_small header__buttton_style_green"
-              >
-                Войти
-              </Link>
-            </>
-          ) : (
-            <div className="header__profile-container">
-              <Link to="/profile" className="header__buttton">
-                Аккаунт
-              </Link>
-              <img
-                className="header__profile-icon"
-                src={profileIcon}
-                alt="иконка кнопки профиля пользователя."
-              />
-            </div>
-          )}
-        </div>
-      </div>
+      </nav>
+      <SideBar isOpen={isSideBarOpen} onClose={closeSideBar} />
     </header>
   );
 }
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,12 +1,9 @@
-import { Link } from "react-router-dom";
 export default function Promo() {
   return (
-    <Link to="/404" style={{ textDecoration: "none", color: "#fff" }}>
-      <section className="promo">
-        <h1 className="promo__title">
-          Учебный проект студента факультета Веб-разработки.
-        </h1>
-      </section>
-    </Link>
+    <section className="promo">
+      <h1 className="promo__title">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+    </section>
   );
 }
